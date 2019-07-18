535c1c77d0456fde6656d1373ffb3f3586ef1428
Prattipati Sai Teja <prteja@deloitte.com>
2019-07-18 12:14:05 +0530

    Create TagGroups

---
+++ CommandStack/Author.Command.Domain/Command/CreateTagGroupsCommand.cs
@@ -0,0 +1,19 @@
+﻿using Author.Command.Domain.Models;
+using MediatR;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Author.Command.Domain.Command
+{
+    public class CreateTagGroupsCommand : IRequest<TagGroupsCommandResponse>
+    {
+        [JsonProperty("languageName")]
+        public List<LanguageName> LanguageName { get; set; }
+    }
+    public class TagGroupsCommandResponse : CommandResponse
+    {
+        [JsonProperty("UpdatedStatus")]
+        public string UpdatedStatus { get; set; }
+    }
+}
