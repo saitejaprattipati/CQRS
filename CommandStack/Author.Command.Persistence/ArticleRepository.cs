df1c2653755183fc53107e382d044196a9e8ab65
Prattipati Sai Teja <a-prteja@deloitte.com>
2020-02-13 19:50:25 +0530

    Create Article

--- CommandStack/Author.Command.Persistence/ArticleRepository.cs
+++ CommandStack/Author.Command.Persistence/ArticleRepository.cs
@@ -2,16 +2,16 @@
 using Author.Command.Persistence.DBContextAggregate;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Author.Core.Framework;
 
 namespace Author.Command.Persistence
 {
     public class ArticleRepository : IArticleRepository
     {
-
         private readonly TaxatHand_StgContext _context;
-
         public IUnitOfWork UnitOfWork
         {
             get
@@ -19,48 +19,107 @@ namespace Author.Command.Persistence
                 return _context;
             }
         }
-
         public ArticleRepository(TaxatHand_StgContext context)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
-
+        public List<Articles> getArticles()
+        {
+            List<Articles> objArticles = _context.Articles.ToList();
+            return objArticles;
+        }
+        public List<TaxTags> getTaxTags()
+        {
+            List<TaxTags> objTaxTags = _context.TaxTags.ToList();
+            return objTaxTags;
+        }
+        public List<Countries> getCountries()
+        {
+            List<Countries> objCountries = _context.Countries.ToList();
+            return objCountries;
+        }
+        public List<CountryGroups> getCountryGroups()
+        {
+            List<CountryGroups> objCountryGroups = _context.CountryGroups.ToList();
+            return objCountryGroups;
+        }
+        public List<Contacts> getContacts()
+        {
+            List<Contacts> objContacts = _context.Contacts.ToList();
+            return objContacts;
+        }
         public Articles Add(Articles Article)
         {
-            //var obj = _context.Images
-            //    .Include(c=>c.Articles)
-            //    .ThenInclude(a=>a.ArticleContents)
-            //    .ToList();
-            //var obj1 = _context.Images
-            //    .ToList();
             return _context.Articles.Add(Article).Entity;
-
         }
+        public int SendNotificationsForArticle(CreateArticleCommand article)
+        {
+            var publishing = article.EventType == "Publish";
 
+            if (publishing && article.NotificationSentDate == null && article.SendNotification && false)
+            {
+                var users = ((from uct in (from user in _context.WebsiteUsers
+                                           from subscribedCountry in user.UserSubscribedCountries
+                                           from subscribedTag in subscribedCountry.UserSubscribedCountryTags.Select(s => s.TaxTag)
+                                           select new UserArticleRelation
+                                           {
+                                               Article = null,
+                                               Country = subscribedCountry.Country,
+                                               TaxTag = subscribedTag,
+                                               WebsiteUser = user
+                                           })
+                              from act in (from carticle in _context.Articles
+                                           from articleTag in carticle.ArticleRelatedTaxTags.Select(at => at.TaxTag)
+                                               //from articleCountry in article.ArticleRelatedCountries
+                                           let articleCountryGroup = carticle.ArticleRelatedCountryGroups.Select(s => s.CountryGroup).SelectMany(s => s.CountryGroupAssociatedCountries)
+                                           let countries = carticle.ArticleRelatedCountries.Select(arc => arc.Country).Concat(articleCountryGroup.Select(g => g.Country))
+
+                                           from country in countries
+                                           where
+                                               /*article.Type == ArticleType.Article
+                                               && */article.IsPublished
+                                               && carticle.PublishedDate <= DateTime.UtcNow
+                                           select new UserArticleRelation
+                                           {
+                                               Article = carticle,
+                                               Country = country,
+                                               TaxTag = articleTag,
+                                               WebsiteUser = null
+                                           })
+                              where
+                                   uct.Country.CountryId == act.Country.CountryId
+                                   && uct.TaxTag.TaxTagId == act.TaxTag.TaxTagId
+                              select new UserArticleRelation
+                              {
+                                  Article = act.Article,
+                                  Country = act.Country,
+                                  TaxTag = act.TaxTag,
+                                  WebsiteUser = uct.WebsiteUser
+                              })
+                .Where(r => r.Article.Type == int.Parse(ArticleType.Article.ToString())
+                            && r.Article.ArticleId == int.Parse(article.ArticleID))
+                .Select(r => r.WebsiteUser)
+                .Distinct()
+                .Include(r => r.UserDevices)).ToList();
+                return users.Count();
+            }
+            return 0;
+        }
         public void Update(Articles order)
         {
             _context.Entry(order).State = EntityState.Modified;
         }
-
-
         public async Task<Articles> GetAsync(int orderId)
         {
             var order = await _context.Articles.FindAsync(orderId);
 
             return order;
         }
-
-
-
     }
-
-
     public interface IArticleRepository : IRepository<Articles>
     {
         Articles Add(Articles order);
-
         void Update(Articles order);
-
         Task<Articles> GetAsync(int orderId);
     }
 }
--- CommandStack/Author.Command.Service/CreateArticleCommandHandler.cs
+++ CommandStack/Author.Command.Service/CreateArticleCommandHandler.cs
@@ -1,13 +1,18 @@
 ﻿//using Microsoft.Extensions.Logging;
 using Author.Command.Domain.Command;
+using Author.Command.Domain.Models;
 using Author.Command.Events;
 using Author.Command.Persistence;
 using Author.Command.Persistence.DBContextAggregate;
+using Author.Core.Framework;
 using MediatR;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace Author.Command.Service
 {
@@ -31,8 +36,6 @@ namespace Author.Command.Service
             _Eventcontext = Eventcontext;
             //    _logger = logger;
         }
-
-
         public async Task<CreateArticleCommandResponse> Handle(CreateArticleCommand request, CancellationToken cancellationToken)
         {
             CreateArticleCommandResponse response = new CreateArticleCommandResponse()
@@ -41,32 +44,183 @@ namespace Author.Command.Service
             };
             try
             {
-                Articles Article = new Articles();
-                Article.State = "Test1";
-                Article.PublishedDate=DateTime.Now;
-                Article.UpdatedDate= DateTime.Now;
-                Article.CreatedDate = DateTime.Now;
-                _ArticleRepository.Add(Article);
-                //  var Status = await _context.CreateArticleDetails();
-                //var eventSourcing = new CreateArticleCommandEvent()
-                //{
-                //    EventType = "ArticleCreation",
-                //    Id = request.Id,
-                //    ArticleName = request.ArticleName,
-                //    ArticleCountry = request.ArticleCountry
-                //};
-                //   using (TransactionScope scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
-                //  {
-                await _ArticleRepository.UnitOfWork
-                   .SaveEntitiesAsync();
-               //     throw new Exception(string.Format("Unable to find a client by id"));
-                    //await _Eventcontext.PublishThroughEventBusAsync(eventSourcing);
+                List<Articles> articles = _ArticleRepository.getArticles();
+                List<TaxTags> taxTags = _ArticleRepository.getTaxTags();
+                List<Countries> countries = _ArticleRepository.getCountries();
+                List<CountryGroups> countryGroups = _ArticleRepository.getCountryGroups();
+                List<Contacts> contacts = _ArticleRepository.getContacts();
+                Articles _article = new Articles();
+                using (TransactionScope scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    _article.Type = request.Type;
+                    _article.Author = request.Author;
+                    _article.PublishedDate = request.PublishedDate.Value;
+                    _article.SubType = request.SubType;
+                    _article.ResourceGroupId = request.ResourceGroupId > 0 ? request.ResourceGroupId : (int?)null;
+                    _article.ResourcePosition = request.ResourcePosition > 0 ? request.ResourcePosition : (int?)null;
+                    _article.IsPublished = request.IsPublished;
+                    _article.CreatedDate = DateTime.UtcNow;
+                    _article.UpdatedDate = DateTime.UtcNow;
+                    _article.ArticleContents = new List<ArticleContents>();
+                    _article.CreatedBy = request.UpdatedBy;
+                    _article.UpdatedBy = request.UpdatedBy;
+                    _article.DisclaimerId = request.DisclaimerId;
+                    _article.ImageId = request.ImageId;
+                    _article.State = request.State;
+                    _article.ProvinceId = request.ProvinceId;
+                    foreach (var acontent in request.ArticleContent)
+                    {
+                        ArticleContents ac = new ArticleContents()
+                        {
+                            LanguageId = acontent.LanguageId,
+                            Content = acontent.Content,
+                            TeaserText = acontent.TeaserText,
+                            Title = acontent.Title
+                        };
+                        _article.ArticleContents.Add(ac);
+                    }
+                    if (request.RelatedArticles != null)
+                    {
+                        var rArticleList = request.RelatedArticles;
+                        var relatedArticles = articles.Where(a => rArticleList.Any(ra => ra == a.ArticleId)).ToList();
+                        foreach (Articles article in relatedArticles)
+                        {
+                            _article.RelatedArticlesArticle.Add(new RelatedArticles()
+                            {
+                                RelatedArticleId = article.ArticleId,
+                                Article = article
+                            });
+                        };
+                        //Add reverse relation
+                        //relatedArticles.ForEach(a => a.RelatedArticlesArticle.Add(new RelatedArticles {Article = Article }));
+                    }
+                    if (request.RelatedResources != null)
+                    {
+                        var rResourceList = request.RelatedResources.ToList();
+                        //var relatedResources = context.Articles.Where(a => rResourceList.Any(ra => ra == a.ArticleId)).ToList();
+                        var relatedResources = articles.Where(a => rResourceList.Contains(a.ArticleId)).ToList();
+                        foreach (Articles article in relatedResources)
+                        {
+                            _article.RelatedResourcesArticle.Add(new RelatedResources()
+                            {
+                                RelatedArticleId = article.ArticleId,
+                                Article = article
+                            });
+                        };
+                    }
+                    if (request.RelatedTaxTags != null)
+                    {
+                        var rTagList = request.RelatedTaxTags.ToList();
+                        //var relatedTags = context.TaxTags.Where(t => rTagList.Any(tt => tt == t.TaxTagId.ToString())).ToList();
+                        var relatedTags = taxTags.Where(t => rTagList.Contains(t.TaxTagId)).ToList();
+                        //Article.ArticleRelatedTaxTags = relatedTags;
+
+                        foreach (TaxTags tags in relatedTags)
+                        {
+                            _article.ArticleRelatedTaxTags.Add(new ArticleRelatedTaxTags()
+                            {
+                                TaxTagId = tags.TaxTagId,
+                                TaxTag = tags
+                            });
+                        };
+                    }
+                    if (request.RelatedCountries != null)
+                    {
+                        var rCountries = request.RelatedCountries.ToList();
+                        //var relatedCountries = context.Countries.Where(c => rCountries.Any(rc => rc == c.CountryId.ToString())).ToList();
+                        var relatedCountries = countries.Where(c => rCountries.Contains(c.CountryId)).ToList();
+                        //articleobject.ArticleRelatedCountries = relatedCountries;
+                        foreach (Countries country in relatedCountries)
+                        {
+                            _article.ArticleRelatedCountries.Add(new ArticleRelatedCountries()
+                            {
+                                CountryId = country.CountryId,
+                                Country = country
+                            });
+                        };
+                    }
+                    if (request.RelatedCountryGroups != null)
+                    {
+                        var rCountryGroups = request.RelatedCountryGroups.ToList();
+                        //var relatedCountryGroups = context.CountryGroups.Where(c => rCountryGroups.Any(rc => rc == c.CountryGroupId)).ToList();
+                        var relatedCountryGroups = countryGroups.Where(c => rCountryGroups.Contains(c.CountryGroupId)).ToList();
+                        // articleobject.ArticleRelatedCountryGroups = relatedCountryGroups;
+                        foreach (CountryGroups cGroup in relatedCountryGroups)
+                        {
+                            _article.ArticleRelatedCountryGroups.Add(new ArticleRelatedCountryGroups()
+                            {
+                                CountryGroupId = cGroup.CountryGroupId,
+                                CountryGroup = cGroup
+                            });
+                        };
+                    }
+                    if (request.RelatedContacts != null)
+                    {
+                        var rContacts = request.RelatedContacts.ToList();
+                        //var relatedContacts = context.Contacts.Where(c => rContacts.Any(rc => rc == c.ContactId)).ToList();
+                        var relatedContacts = contacts.Where(c => rContacts.Contains(c.ContactId)).ToList();
+                        // articleobject.ArticleRelatedContacts = relatedContacts;
+                        foreach (Contacts rcontact in relatedContacts)
+                        {
+                            _article.ArticleRelatedContacts.Add(new ArticleRelatedContacts()
+                            {
+                                ContactId = rcontact.ContactId,
+                                Contact = rcontact
+                            });
+                        };
+                    }
+                    //Push logic needs to be implemented
+                    int userCount = _ArticleRepository.SendNotificationsForArticle(request);
+                    if (userCount > 0) { _article.NotificationSentDate = DateTime.Now; }
+                    _ArticleRepository.Add(_article);
+                    await _ArticleRepository.UnitOfWork
+                       .SaveEntitiesAsync();
+
                     response.IsSuccessful = true;
-
-                //    scope.Complete();
-             //   }
-            return response;
-
+                    scope.Complete();
+                }
+                using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    foreach (var content in _article.ArticleContents)
+                    {
+                        var eventSourcing = new ArticleCommandEvent()
+                        {
+                            EventType = ServiceBusEventType.Create,
+                            ArticleId = _article.ArticleId,
+                            PublishedDate = _article.PublishedDate,
+                            Author = _article.Author,
+                            ImageId = _article.ImageId,
+                            State = _article.State,
+                            Type = _article.Type,
+                            SubType = _article.SubType,
+                            ResourcePosition = _article.ResourcePosition,
+                            DisclaimerId = _article.DisclaimerId,
+                            ResourceGroupId = _article.ResourceGroupId,
+                            IsPublished = _article.IsPublished,
+                            CreatedDate = _article.CreatedDate,
+                            CreatedBy = _article.CreatedBy,
+                            UpdatedDate = _article.UpdatedDate,
+                            UpdatedBy = _article.UpdatedBy,
+                            NotificationSentDate = _article.NotificationSentDate,
+                            ProvinceId = _article.ProvinceId,
+                            ArticleContentId = content.ArticleContentId,
+                            LanguageId = content.LanguageId,
+                            Title = content.Title,
+                            TeaserText = content.TeaserText,
+                            Content = content.Content,
+                            RelatedContacts = _article.ArticleRelatedContacts.Select(s => new RelatedEntityId { IdVal = s.ContactId }).ToList(),
+                            RelatedCountries = _article.ArticleRelatedCountries.Select(s => new RelatedEntityId { IdVal = s.CountryId }).ToList(),
+                            RelatedCountryGroups = _article.ArticleRelatedCountryGroups.Select(s => new RelatedEntityId { IdVal = s.CountryGroupId }).ToList(),
+                            RelatedTaxTags = _article.ArticleRelatedTaxTags.Select(s => new RelatedEntityId { IdVal = s.TaxTagId }).ToList(),
+                            RelatedArticles = _article.RelatedArticlesArticle.Select(s => new RelatedEntityId { IdVal = s.ArticleId }).ToList(),
+                            RelatedResources = _article.RelatedResourcesArticle.Select(s => new RelatedEntityId { IdVal = s.RelatedArticleId }).ToList(),
+                            Discriminator = Constants.ArticlesDiscriminator
+                        };
+                        await _Eventcontext.PublishThroughEventBusAsync(eventSourcing);
+                    }
+                    scope.Complete();
+                }
+                return response;
             }
             catch (Exception ex)
             {
